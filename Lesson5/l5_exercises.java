32ac82f2ec152673a05742d7baa30a67183ecc65
Grizzesz <38742224+Grizzesz@users.noreply.github.com>
2019-09-23 15:50:22 -0700

    Uploaded Lesson 5 Project

---
+++ Lesson5/l5_exercises.java
@@ -0,0 +1,128 @@
+class Testr {
+    public static void main(String[] args) {
+
+        
+
+    }
+
+    private static void question1() {
+
+        // Question 1:
+
+        final double E = 2.718;
+
+    }
+
+    private static void question2() {
+
+        // Question 2:
+
+        final int NUM_STUDENTS = 236;
+
+    }
+
+    private static void question3() {
+
+        // Question 3:
+
+        final double Area;
+        Area = 203.49;
+
+        // Area cannot be assigned if already declared as a constant
+
+    }
+
+    private static void question4() {
+
+        // Question 4:
+
+        int cnt = 27.2;
+        System.out.println(cnt);
+
+    }
+
+    private static void question5() {
+
+        // Question 5:
+
+        double d = 78.1;
+        int fg = (int)d;
+        System.out.println(fg);
+
+    }
+
+    private static void question6() {
+
+        // Question 6:
+
+        fouble f4 = 22;
+        
+        // legal
+
+    }
+
+    private static void question7() {
+
+        // Question 7:
+
+        double j = (double)61/3;
+        System.out.println(j);
+
+    }
+
+    private static void question8() {
+
+        // Question 8:
+
+        System.out.println( (double)(90/9) ); 
+
+    }
+
+    private static void question9() {
+
+        // Question 9:
+
+        System.out.println(4 + 6.0/4 + 5 * 3 - 3);
+
+    }
+
+    private static void question10() {
+
+        // Question 10:
+
+        int p = 3;
+        double d = 10.3;
+        int j = (int)5.9;
+        System.out.println(p + p * d - 3 * j);
+
+    }
+
+    private static void question11() {
+
+        // Question 11:
+
+        int p = 3;
+        double d = 10.3;
+        int j = (int)5.9;
+        System.out.println(p + p * (int)d - 3 * j);
+
+    }
+
+    private static void questions12_15() {
+
+        // Questions 12 - 15:
+
+        int dividend = 12, divisor = 4, quotient = 0, remainder = 0;
+        int dividend2 = 13, divisor2 = 3, quotient2 = 0, remainder2 = 0;
+        quotient = dividend/divisor;
+        remainder = dividend % divisor;
+        quotient2 = dividend2 / divisor2;
+        remainder2 = dividend2 % divisor2;
+
+        System.out.println(quotient);
+        System.out.println(remainder);
+        System.out.println(quotient2);
+        System.out.println(remainder2);
+        
+    }
+}
